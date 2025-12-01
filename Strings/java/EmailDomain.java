87ae79e0797c6df228cc24887db1d4bf900a3b9c
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2025-12-01 16:11:54 -0300

    feat: Email Domain problem

---
+++ Strings/java/EmailDomain.java
@@ -0,0 +1,25 @@
+
+import entities.EmailInfo;
+
+public class EmailDomain {
+    public static void main(String[] args) {
+        String email1 = "yu_nakamura@gmail.com";
+        String email2 = "jose_Carlos@yahoo.com.br";
+        String email3 = " marcos@marcos.br";
+        String email4 = "chie_Stk@pm.me";
+
+        System.out.println(extractEmailInformation(email1).isBrazilianDomain());
+        System.out.println(extractEmailInformation(email2).isBrazilianDomain());
+        System.out.println(extractEmailInformation(email3).isBrazilianDomain());
+        System.out.println(extractEmailInformation(email4).isBrazilianDomain());
+    }
+
+    public static EmailInfo extractEmailInformation(String email) {
+        /*
+        *   Method that extracts the user, domain and nationality
+        */
+        String[] emailData = email.split("@");
+        
+        return new EmailInfo(emailData[0], emailData[1]);
+    }
+}
