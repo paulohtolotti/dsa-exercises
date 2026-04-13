46c28674447f5042e62ec1c2efa4dce75897c4c6
paulohtolotti <paulohtolotti@alumni.usp.br>
2026-04-13 13:39:04 -0300

    feat: exercício visitantes set

---
+++ Conjuntos/js/visitantes.mjs
@@ -0,0 +1,26 @@
+import { dataset } from './dados.mjs';
+
+function calculateUniqueVisitors(data) {
+    let set = new Set();
+
+    for(const d of data) {
+        let visitor = d.split(",")[0];
+        set.add(visitor);
+    }
+
+    return set.size;
+}
+
+function scenario(data) {
+    const size = calculateUniqueVisitors(data);
+    process.stdout.write(`${size} unique visitors\n`);
+}
+const data = [
+    "ana,2024-07-04T21:42:40.353283800Z,https://blog.com/login",
+    "bob,2024-07-04T21:42:44.571283800Z,https://blog.com/news",
+    "maria,2024-07-04T21:42:46.394283800Z,https://blog.com/shop",
+    "ana,2024-07-04T21:42:50.026283800Z,https://blog.com/news"
+];
+
+scenario(data);
+scenario(dataset);
